9efad8cf11c4e1149cad2493a5994484aa4ecd13
Alex Fernandes <alex.fernandes@mov.ai>
2022-04-26 16:02:53 +0100

    add GD_node

--- scripts/base/create-local-apt-registry.sh
+++ scripts/base/create-local-apt-registry.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#
+# Copyright 2021 MOV.AI
+#
+#    Licensed under the Mov.AI License version 1.0;
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.mov.ai/flow-license/
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
 REGISTRY_PATH="/usr/local/apt-registry"
 
 RELOAD_REGISTRY_SCRIPT_NAME="reload-local-debs.sh"
@@ -20,5 +35,5 @@ bash "${RELOAD_REGISTRY_SCRIPT_PATH}/${RELOAD_REGISTRY_SCRIPT_NAME}"
 chown 1000:1000 "${REGISTRY_PATH}/Packages.gz"
 
 # Have apt use our local registry
-echo -e "deb [trusted=yes] file:${REGISTRY_PATH} ./ \n$(cat /etc/apt/sources.list)" > /etc/apt/sources.list 
+echo -e "deb [trusted=yes] file:${REGISTRY_PATH} ./ \n$(cat /etc/apt/sources.list)" > /etc/apt/sources.list
 
