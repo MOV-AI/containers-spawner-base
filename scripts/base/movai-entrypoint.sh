cd76f490b2521d64026006058522f514f37d0c1b
Alex Fernandes <84147256+AlexFernandes-MOVAI@users.noreply.github.com>
2022-04-27 10:25:48 +0100

    3.0 Spawner (#27)
    * DP-647 : Update to latest base
    
    * Main to dev (#25)
    
    * DP-647 : Update to latest base (#21)
    
    * DP-521 : metadata init (#22)
    
    * DP-647 : Update to latest base
    
    * add env var test
    
    * Feat : switch to new backend modules (#26)
    
    * prepare switch
    
    * review whole script to use new scritps
    
    * cicd for dev
    
    * cicd for dev
    
    * update registry
    
    * correct symlink
    
    * clean comments
    
    * update flow-initator version
    
    * Fix : ce new spawner (#28)
    
    * add GD_node
    
    * add license
    
    * Fix: licenses, GD_node.py and IGN (#29)
    
    * add GD_node
    
    * add license
    
    * update flow-initiator
    
    * fix cp
    
    * switch docker ci branch
    
    * add ign spawner
    
    * fix hadolint
    
    * still compile for melodic
    
    * add dependencies in workflows
    
    * restore main cicd branch
    
    * bump flow-initiator
    
    * bump flow initiator

--- scripts/base/movai-entrypoint.sh
+++ scripts/base/movai-entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#
+# Copyright 2021 MOV.AI
+#
+#    Licensed under the Mov.AI License version 1.0;
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.mov.ai/flow-license/
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
 # File: movai-entrypoint.sh
 set -e
 printf "Mov.ai Spawner - %s Edition\n" "$MOVAI_ENV"
@@ -36,7 +51,7 @@ fi
 
 # Launch spawner init db tool
 echo "Info : initializing local DB ..."
-/usr/bin/python3 -m tools.init_local_db >/dev/null &
+/usr/bin/python3 -m flow_initiator.tools.init_local_db >/dev/null &
 echo "Info : initializing local DB. DONE"
 
 # First run metadata initializations
@@ -62,4 +77,4 @@ if [ ! -f "${MOVAI_HOME}/.first_run_metadata" ] && [ "$UPDATE_MASTER_METADATA" =
     popd > /dev/null
 fi
 
-"${APP_PATH}"/async_movaicore.py -v
+/usr/bin/python3 -m flow_initiator
